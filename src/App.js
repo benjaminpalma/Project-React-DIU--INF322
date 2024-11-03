54a615d0d73c858570fca1534f13f2fb888f1825
benjaminpalma <benjamin.palmar@usm.cl>
2024-11-03 02:45:26 -0300

    Cleaning the repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Communities from './pages/communities/Communities';
-import HobbiesPage from './pages/hobbies_page/HobbiesPage';
-import HomePage from './pages/home_page';
+import HobbiesPage from './pages/hobbies/HobbiesPage';
+import HomePage from './pages/HomePage';
 import Navbar from './components/Navbar';
-//import Login from './login_signup/login';
-//import Signup from './login_signup/signup';
-//import Profile from './pages/profile_page/profile_page';
+import Login from './pages/login/Login';
+//import Signup from './pages/login/Signup';
+//import Profile from './pages/profile/Profile';
 
 const App = () => {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-1 p-6">
+	<header className="fixed inset-x-0 top-0 z-50 bg-white">
+          <Navbar />
+        </header>
+        <main className="flex-1 p-6 mt-16">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/communities" element={<Communities />} />
             <Route path="/hobbies" element={<HobbiesPage />} />
-	  {/* <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+	    <Route path="/login" element={<Login />} />
+	  {/*<Route path="/signup" element={<Signup />} />
             <Route path="/profile" element={<Profile />} /> */}
           </Routes>
         </main>
